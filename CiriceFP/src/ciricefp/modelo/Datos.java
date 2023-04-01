c23d46a39e5d64f20d5b695576086db92e3edbda
katiane-br <117689006+katiane-br@users.noreply.github.com>
2023-04-01 19:32:29 +0200

    Update Datos

--- CiriceFP/src/ciricefp/modelo/Datos.java
+++ CiriceFP/src/ciricefp/modelo/Datos.java
@@ -2,6 +2,12 @@ package ciricefp.modelo;
 
 import ciricefp.controlador.*;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Date;
+
 /**
  * Esta clase funciona como un controlador interno para el módulo Modelo siguiendo el patrón MVC.
  *
@@ -74,6 +80,134 @@ public class Datos {
     public void setPedido(Listas<Pedido> pedido) {
         this.pedido = pedido;
     }
+
+    //Cambiada a Void porque add no devuelve nada
+    public void createArticulo(Articulo articulo) {
+        this.articulos.add(articulo);
+    }
+
+    public ArrayList<Articulo> listArticulos(){
+        return this.articulos.getLista();
+    }
+
+    public Articulo searchArticulo (String codigo){
+        for(Articulo art : this.articulos.getLista()) {
+           if( codigo==art.getCodArticulo() ){
+               return art;
+           }
+        }
+        return null;
+    }
+
+    //Cambiada a Void porque clear no devuelve nada
+    public void clearArticulos (){
+        this.articulos.clear();
+    }
+
+    //Eliminar articulo función no consta Uml
+
+    public void removeArticulo (Articulo articulo){
+        this.articulos.borrar(articulo);
+    }
+
+    //Cambiada a Void porque add no devuelve nada
+    public void createCliente(Cliente cliente) {
+        this.clientes.add(cliente);
+    }
+    public ArrayList<Cliente> listClientes(){
+        return this.clientes.getArrayList();
+    }
+    public ArrayList<Cliente> filterClientesByType(String tipo){
+        ArrayList<Cliente> clienteFiltered = new ArrayList<Cliente>();
+        for( Cliente cl: this.listClientes() ) {
+            if( tipo == cl.tipoCliente()){
+                clienteFiltered.add(cl);
+            }
+        }
+        return clienteFiltered;
+    }
+    public Cliente searchCliente (String dni){
+        for( Cliente ok : this.clientes.getLista()) {
+            if( dni == ok.getNif()){
+                return ok;
+            }
+        }
+        return null;
+    }
+    //Cambiada a Void porque clear no devuelve nada
+    public void clearClientes (){
+        this.clientes.clear();
+    }
+
+    //Eliminar articulo función no consta Uml
+
+    public void removeCliente (Cliente cliente){
+        this.clientes.borrar(cliente);
+    }
+    //Cambiada a Void porque add no devuelve nada
+    public void createPedido(Pedido pedido) {
+        this.pedido.add(pedido);
+    }
+    public ArrayList<Pedido> listPedidos(){
+        return this.pedido.getArrayList();
+    }
+    public void deletePedido (Pedido pedido){
+        this.pedido.borrar(pedido);
+    }
+    public Pedido searchPedido (int numeroPedido){
+        for( Pedido order : this.pedido.getLista()) {
+            if( numeroPedido == order.getNumeroPedido()){
+                return order;
+            }
+        }
+        return null;
+    }
+    public ArrayList<Pedido> filterPedidoByCliente(String dni){
+        ArrayList<Pedido> pedidosFiltered = new ArrayList<Pedido>();
+        for( Pedido order: this.pedido.getLista() ) {
+            if( dni == order.getCliente().getNif()){
+                pedidosFiltered.add(order);
+            }
+        }
+        return pedidosFiltered;
+    }
+    public ArrayList<Pedido> filterPedidoEnviadoByFecha (LocalDate fecha){
+        ArrayList<Pedido> pedidosFiltered = new ArrayList<Pedido>();
+        for( Pedido order: this.pedido.getLista() ) {
+            if( fecha.format( DateTimeFormatter.ofPattern("dd/MM/yyyy") ) == order.getFechaPedido().format( DateTimeFormatter.ofPattern("dd/MM/yyyy")) ){
+                pedidosFiltered.add(order);
+            }
+        }
+        return pedidosFiltered;
+    }
+
+    public ArrayList<Pedido> filterPedidoByEstado(String opt){
+        ArrayList<Pedido> estadoFiltered = new ArrayList<Pedido>();
+        for( Pedido order: this.pedido.getLista() ) {
+            switch (opt) {
+                case "Enviado":
+                    //Enviado
+                    if( order.getEsEnviado() ){
+                        estadoFiltered.add(order);
+                    }
+                    break;
+                case "No Enviado":
+                    //No enviado
+                    if( !order.getEsEnviado() ){
+                        estadoFiltered.add(order);
+                    }
+                    break;
+            }
+
+        }
+        return estadoFiltered;
+    }
+    //Cambiada a Void porque clear no devuelve nada
+    public void clearPedidos (){
+        this.pedido.clear();
+    }
+
+
 
     @Override
     public String toString() {
